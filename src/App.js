7357247d0a687e11a9a949c9ccc7545eb7a5d872
MTamPham <mtam78199@gmail.com>
2021-03-28 09:41:39 +0100

    Commit useState() labs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
 import './App.scss';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Take a bath' },
+    { id: 2, title: 'Eat breakfast' },
+    { id: 3, title: 'Go to work' },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('form submit:', formValues);
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1, // dummy ID
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>Welcome to React Hooks!</h1>
+
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
